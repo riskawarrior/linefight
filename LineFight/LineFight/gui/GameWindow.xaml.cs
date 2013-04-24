e4323987b7114d79c9c0273a570ffd9629f55974
CzeZ <czez90@gmail.com>
2013-04-24 12:12:30 +0200

    Game + GameWindow + WriteableBitmapEx

---
+++ LineFight/LineFight/gui/GameWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using System.Windows.Media.Imaging;
+using LineFight.model;
+using UniversalNetwork;
+
+namespace LineFight.gui
+{
+    public partial class GameWindow : Window
+    {
+        private Image Arena;
+        private Button btnAbandon;
+        private Game Controller;
+        private DispatcherTimer CountDown;
+        private Image ImgMyProfile;
+        private Image ImgOpponentProfile;
+        private Label lblCountdown;
+        private Profile MyProfile;
+        private LFNet Network;
+        private Profile OpponentProfile;
+        private DispatcherTimer Refresher;
+        private int Remaining;
+        private bool Replay;
+
+        private void btnAbandon_Click(object o, RoutedEventArgs e)
+        { 
+            
+        }
+
+        private void CountDown_Tick()
+        { 
+        
+        }
+
+        public GameWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void GameWindow_KeyPress(object o, KeyboardEventArgs e)
+        { 
+        
+        }
+
+        private void NetClientEventHandler(object o, NetClientEvent e)
+        { 
+            
+        }
+
+        public void NetPackageReceiveHandler(object o, PackageReceived pr)
+        { 
+        
+        }
+
+        public void NewGame()
+        {
+            Controller = new Game();
+            Controller.Start();
+            gameImg.Source = Controller.getArena();
+            Controller.draw();
+            //Controller.getArena().GetBitmapContext();
+            gameImg.UpdateLayout();
+        }
+
+        private void Refresher_Tick(object o, EventArgs e)
+        { 
+            
+        }
+    }
+}
---
+++ LineFight/LineFight/model/Game.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.Windows.Media.Imaging;
+using UniversalNetwork;
+
+namespace LineFight.model
+{
+    enum Facing {Up, Down, Left, Right};
+
+    class Game
+    {
+        private WriteableBitmap Arena;
+        private Facing Facing;
+        private bool Lost;
+        private DispatcherTimer Mover;
+        private Color MyColor;
+        private LFNet Network;
+        private Color OpponentColor;
+        private Facing OpponentFacing;
+        private Point Position;
+        private int Speed;
+        private bool Win;
+
+        public WriteableBitmap getArena()
+        {
+            return Arena;
+        }
+
+        public bool IsEnded()
+        {
+        
+            return false;
+        }
+
+        public bool IsWin()
+        {
+            return false;
+        }
+
+        public void Mover_Tick(object o, EventArgs e)
+        {
+        
+        }
+
+        private void NetClientEventHandler(object o, NetClientEvent e)
+        {
+        
+        }
+
+        private void NetPackageReceiveHandler(object o, PackageReceived pr)
+        { 
+        
+        }
+
+        private void SendFacing(Facing f)
+        {
+        
+        }
+
+        public void Start()
+        {
+            Arena = BitmapFactory.New(500, 500);
+            //Arena.GetBitmapContext();
+        }
+
+        public void draw()
+        {
+            //Arena.GetBitmapContext();
+            Arena.Clear(Colors.Aquamarine);
+            Arena.SetPixel(5, 5, Colors.Blue);
+            Arena.SetPixel(495, 5, Colors.Red);
+            Arena.DrawEllipse(200, 200, 400, 400, Colors.Black);
+            
+        }
+
+        public void TurnLeft()
+        {
+        
+        }
+
+        public void TurnRight()
+        {
+        
+        }
+    }
+}
